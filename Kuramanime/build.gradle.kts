5a363dd3b44f9d2e5edb383478c7f6b783c73693
kasanetetouwu0401 <kasanetetouwu0401@gmail.com>
2026-04-29 05:11:36 +0700

    Last Sync: 2026-04-29 05:11 (Mobile)

--- Kuramanime/build.gradle.kts
+++ Kuramanime/build.gradle.kts
@@ -12,12 +12,3 @@ cloudstream {
 
     iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://v17.kuramanime.ink&size=%size%"
 }
-
-// Tambahkan blok dependencies ini di bagian bawah ya sayang! 👇
-dependencies {
-    // Pastikan dependensi bawaan project CloudStream tetap ada
-    implementation(project(":Cloudstream"))
-    
-    // Ini senjata rahasia kita dari Miku! (Rhino JavaScript Engine)
-    implementation("org.mozilla:rhino:1.7.14")
-}
